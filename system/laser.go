3c4ea198ccaec5097e019f96673d7acf0270c3a8
Guillaume Boulant <gboulant@gmail.com>
2025-08-31 21:36:45 +0200

    make the LaserSystem implement the System interface and have exported attributes

--- system/laser.go
+++ system/laser.go
@@ -27,33 +27,46 @@ We set Z=w*t to have an autonomous system:
 // approximation of the rates equations balancing the light intensity L with the
 // population inversion D.
 type LaserSystem struct {
-	m, g, w, a float64
+	M, G, W, A float64
 }
 
-// f implements the function f of the laser system (in dX/dt = f(X,t))
-func (dynsys LaserSystem) f(t float64, X []float64) ([]float64, error) {
+// F implements the function F of the laser system (in dX/dt = F(X,t))
+func (dynsys LaserSystem) F(t float64, X []float64) ([]float64, error) {
 	L := X[0]
 	D := X[1]
 	Z := X[2]
 
-	dL := D - 1 - dynsys.m*math.Cos(Z)
-	dD := dynsys.g * (dynsys.a - D*(1+math.Exp(L)))
-	dZ := dynsys.w
+	dL := D - 1 - dynsys.M*math.Cos(Z)
+	dD := dynsys.G * (dynsys.A - D*(1+math.Exp(L)))
+	dZ := dynsys.W
 	return []float64{dL, dD, dZ}, nil
+}
+
+func (dynsys LaserSystem) GetDefaultInput() (t0 float64, X0 []float64, step float64, tmax float64) {
+	L0 := 1.0
+	D0 := 1.0
+	Z0 := 0.0
+	t0 = 0.0
+	X0 = []float64{L0, D0, Z0}
+
+	T := 2 * math.Pi / dynsys.W
+	tmax = 60 * T
+	step = T / 40
+	return t0, X0, step, tmax
 }
 
 var configurations = map[string]LaserSystem{
 	"chaos": {
-		m: 2.5e-2,
-		g: 1e-3,
-		w: 1e-2,
-		a: 1.1,
+		M: 2.5e-2,
+		G: 1e-3,
+		W: 1e-2,
+		A: 1.1,
 	},
 	"1T_LONG_TRANSIENT": {
-		m: 2.5e-2,
-		g: 1e-3,
-		w: 1e-1,
-		a: 1.1,
+		M: 2.5e-2,
+		G: 1e-3,
+		W: 1e-1,
+		A: 1.1,
 	},
 }
 
@@ -68,13 +81,13 @@ func DemoLaser(postpro bool) error {
 	t0 := 0.0
 	X0 := []float64{L0, D0, Z0}
 
-	T := 2 * math.Pi / dynsys.w
+	T := 2 * math.Pi / dynsys.W
 	tmax := 60 * T
 	h := T / 40
 
 	algo := solver.NewRK4Solver()
 	var recorder solver.RecorderTimeSeries
-	_, err := algo.Solve(dynsys.f, t0, X0, h, solver.StopAtTime(tmax), &recorder)
+	_, err := algo.Solve(dynsys.F, t0, X0, h, solver.StopAtTime(tmax), &recorder)
 	if err != nil {
 		return err
 	}
@@ -108,12 +121,12 @@ func DemoLaserFirstReturnMap(postpro bool) error {
 	t0 := 0.0
 	X0 := []float64{L0, D0, Z0}
 
-	T := 2 * math.Pi / dynsys.w
+	T := 2 * math.Pi / dynsys.W
 	h := T / 40
 
 	// We first let the transient behaviour stabilized on the attractor
 	algo := solver.NewRK4Solver()
-	_, err := algo.Solve(dynsys.f, t0, X0, h, solver.StopAtTime(100*T), nil)
+	_, err := algo.Solve(dynsys.F, t0, X0, h, solver.StopAtTime(100*T), nil)
 	if err != nil {
 		return err
 	}
@@ -124,7 +137,7 @@ func DemoLaserFirstReturnMap(postpro bool) error {
 	timeseries.Append(solver.NewTimeData(0, X))
 
 	for i := 0; i < 10000; i++ {
-		_, err := algo.Solve(dynsys.f, 0, X, h, solver.StopAtTime(T), nil)
+		_, err := algo.Solve(dynsys.F, 0, X, h, solver.StopAtTime(T), nil)
 		if err != nil {
 			return err
 		}
